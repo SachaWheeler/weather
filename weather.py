f8a67fd6343db727992838f85df108a9a5db6058
Sacha Wheeler <sacha@sachawheeler.com>
2024-07-26 09:22:14 +0100

    remove inflect engine

--- weather.py
+++ weather.py
@@ -4,21 +4,16 @@ import requests
 import json
 import time
 import pprint
-import inflect
 import datetime
 from num2words import num2words
-
-import os.path
-from licence import API_KEY
-
 import re
+import os.path
 
+from licence import API_KEY
 from google_auth import authenticate_google_calendar, get_today_upcoming_events
 
 
 def extract_time(start):
-    """Extract the time from the datetime string."""
-    # Using regular expression to find the time part
     match = re.search(r'T(\d{2}:\d{2}):\d{2}', start)
     if match:
         return match.group(1)
@@ -36,7 +31,6 @@ def degToCompass(num):
 time.sleep(10)
 now = datetime.datetime.now()
 def get_date_str():
-    # date_str = datetime.datetime.now().strftime('%A, %-d  %B')
     hour = num2words(now.strftime('%I'), lang="en")
     day = now.strftime('%A')
     date = num2words(now.strftime('%-d'), lang="en", to="ordinal")
@@ -44,32 +38,30 @@ def get_date_str():
     return f"{hour} o clock on {day} the {date} of {month}"
 
 def get_time_str(time, twentyfour_hour=False):
-    # time = datetime.datetime.fromtimestamp(timestamp)
     time = datetime.datetime.strptime(time, "%H:%M")
     BST = 0  # 1  # make zero again when BST ends
     if twentyfour_hour:
         full_hour = (int(time.strftime('%I')) + BST)
     else:
         full_hour = (int(time.strftime('%I')) + BST) % 12
-    hour = p.number_to_words(full_hour)
+    # hour = p.number_to_words(full_hour)
+    hour = num2words(full_hour, lang="en")
     minute = int(time.strftime('%M'))
     if minute == 0:
         minute = "oh clock"
     elif minute < 10:
-        minute = " oh " + p.number_to_words(minute)
+        minute = " oh " + num2words(minute, lang="en")
     else:
-        minute = p.number_to_words(minute)
+        minute = num2words(minute, lang="en")
     string = f"{hour} {minute}"
     return string
 
+# get Weather
 API_DOMAIN = "http://api.weatherapi.com/v1/forecast.json"
 API_URL = f"{API_DOMAIN}?key={API_KEY}&q=London&days=1&aqi=no&alerts=no"
 
 response = requests.get(API_URL)
 data = json.loads(response.text)
-# pprint.pprint(data)
-
-p = inflect.engine()
 
 current = data['current']
 date_str = get_date_str()
@@ -81,30 +73,33 @@ elif current_hour < 18:
 else:
     day_stage = "evening"
 
-current_temp = f"{p.number_to_words(int(current['temp_c']))} degrees"
+# current_temp
+current_temp = f"{num2words(int(current['temp_c']), lang='en')} degrees"
 if int(current['temp_c']) != int(current['feelslike_c']):
-    current_temp += f" but feels like {p.number_to_words(int(current['feelslike_c']))}"
+    current_temp += f" but feels like {num2words(int(current['feelslike_c']), lang='en')}"
 
-wind_speed = p.number_to_words(int(float(current['wind_kph']) * 1000 / (60 * 60)))  # km/h -> m/s
+# Wind speed
+wind_speed = num2words(int(float(current['wind_kph']) * 1000 / (60 * 60)), lang="en")  # km/h -> m/s
 wind_direction = degToCompass(current['wind_degree'])
 
 conditions = current['condition']['text']
 
+# Temp forecast
 today = data['forecast']['forecastday'][0]
-high = p.number_to_words(f"{today['day']['maxtemp_c']:0.0f}")
-low = p.number_to_words(f"{today['day']['mintemp_c']:0.0f}")
+high = num2words(f"{today['day']['maxtemp_c']:0.0f}", lang="en")
+low = num2words(f"{today['day']['mintemp_c']:0.0f}", lang="en")
 temp_forecast = f"A high of {high} and a low of {low} degrees Celcius"
 
+# Sunset
 sunset = get_time_str(today['astro']['sunset'].split(" ")[0])
 sunset_time = datetime.datetime.strptime(today['astro']['sunset'], "%I:%M %p")
 sunrise_time = datetime.datetime.strptime(today['astro']['sunrise'], "%I:%M %p")
 
 secs_of_day = sunset_time - sunrise_time
 hours_of_day, mins_of_day, _ = str(secs_of_day).split(":")
-# print(hours_of_day, mins_of_day)
-hours_of_day_str = p.number_to_words(hours_of_day) + " hours"
+hours_of_day_str = num2words(hours_of_day, lang="en") + " hours"
 if int(mins_of_day) > 0:
-    hours_of_day_str += f" and {p.number_to_words(mins_of_day)} minutes"
+    hours_of_day_str += f" and {num2words(mins_of_day, lang='en')} minutes"
 
 hourly = today['hour']
 
@@ -131,17 +126,17 @@ for h in hourly:
     # print(hour, desc, pop)
     if int(hour) == 23: # only look at today
         break
-    # print(rain_change, h['time_epoch'], h['time'])
 
+# Rain
 if rain_change is not None:
-    preface = f"{rain_desc} expected at {p.number_to_words(rain_change)}"
+    preface = f"{rain_desc} expected at {num2words(rain_change, lang='en')}"
     if int(hour) > 13:
         suffix = "hundred hours"
     else:
         suffix = "o clock"
     rain_prediction = f"{preface} {suffix}"
 
-# get appointments
+# get Calendar appointments
 service = authenticate_google_calendar()
 appointments = get_today_upcoming_events(service)
 
